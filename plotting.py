826878c4bd7b545ea532a8536f6940d3d3f56323
JeremyElvander <jeremyelvander@gmail.com>
2026-03-11 19:43:54 -0700

    Documentation

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 #Helper file to reduce clutter in main ipynb
+#Import dependencies
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, LinearSegmentedColormap, ListedColormap
 import matplotlib.patches as mpatches
@@ -17,42 +18,63 @@ import xarray as xr
 import rioxarray
 
 def static_plot(shipping_raster=None, ice_raster = None, shipping=False, ice=False):
+    '''
+    Function that produces a static plot for shipping/ice concentration in the artic.
+
+    :param shipping_raster: raster file for AIS data
+    :param ice_raster: raster file for ice concentration
+    :param shipping: Boolean to include shipping
+    :param ice: boolean to include ice
+
+    :return: figure and ax matplotlib objects
+    '''
+    #Retrieve shipping bounds (ice concentration is much larger raster)
     with rio.open('data/ais_traffic/arctic_maritime_2020.tif') as src:
         bounds = src.bounds
 
+    #Retrieve Cartopy projection for arctic, extent
     proj = ccrs.NorthPolarStereo(central_longitude=0, true_scale_latitude=71)
     extent = [bounds.left, bounds.right, bounds.bottom, bounds.top]
-    norm = LogNorm(vmin=0.0001, vmax=2000)
 
+    #Create fig and ax objects and set ax limits, provide CCRS projection for cartopy features
     fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': proj})
     ax.set_xlim(bounds.left, bounds.right)
     ax.set_ylim(bounds.bottom, bounds.top)
 
-
+    #Add relevant features for basemap manually (ocean, land, coastline, borders)
     ax.add_feature(cfeature.OCEAN.with_scale('50m'), facecolor="#717171", edgecolor='none', zorder=0)
     ax.add_feature(cfeature.LAND.with_scale('50m'), facecolor='#0e0e0e', edgecolor='none', zorder=1)
     ax.add_feature(cfeature.COASTLINE.with_scale('50m'), edgecolor='#333333', linewidth=0.5, zorder=2)
     ax.add_feature(cfeature.BORDERS.with_scale('50m'), edgecolor='#333333', linewidth=0.3, zorder=2)
 
+    #If ice raster provided
     if ice:
+        #Add ice raster, custom normalizer for cbar (percentage)
         im_ice = ax.imshow(ice_raster, cmap='Blues_r', extent=extent, norm=plt.Normalize(vmin=0,vmax=100), zorder=3)
         #Ice cbar
+        #Reposition ice cbar if shipping cbar is also present
         if shipping:
             fig.canvas.draw()
+            #Set position manually
             ax.set_position([0.02, 0.02, 0.84, 0.96]) 
 
             ice_cbar_ax = fig.add_axes([0.87, 0.52, 0.025, 0.40])
             ice_cbar = fig.colorbar(im_ice, cax=ice_cbar_ax)
+        #Otherwise use default extent for ice CBAR
         else:
             ice_cbar = fig.colorbar(im_ice, ax=ax, shrink=0.7)
         ice_cbar.set_label('Ice Concentration (%)', fontsize=12)
 
+    #If shipping raster is present
     if shipping:
+        #Create log scale for cbar based on GMTDS scale
         norm = LogNorm(vmin=0.0001, vmax=2000)
+        #Add shipping image
         im_shipping = ax.imshow(shipping_raster, cmap='Reds_r', extent=extent, norm=norm, zorder=3)
 
-        #Shipping cbar
+        #Shipping cbar with custom tick placement
         cbar_ticks = [0.001, 0.05, 1, 2, 5, 10, 20, 100, 2000]
+        #Adjust cbar if ice is present otherwise use default settings
         if ice:
             cbar_ax = fig.add_axes([0.87, 0.08, 0.025, 0.40])
             cbar = plt.colorbar(im_shipping, cax=cbar_ax, ticks=cbar_ticks, shrink=0.7)
@@ -60,67 +82,88 @@ def static_plot(shipping_raster=None, ice_raster = None, shipping=False, ice=Fal
             cbar = plt.colorbar(im_shipping, ax=ax, ticks=cbar_ticks, shrink=0.7)
         cbar.ax.set_yticklabels([str(i) for i in cbar_ticks])
         cbar.set_label('AIS Density', fontsize=12)
-    
+    #Return figure and ax
     return fig, ax
 
 
 
 def dynamic_plot(years, bounds, interval, shipping=True, ice=False, save=False, filename='series.gif'):
-    
-    # with rio.open('data/ais_traffic/arctic_maritime_2020.tif') as src:
-    #     img_arr = src.read(1)
+    '''
+    Function to create (and optionally save) gif of arctic shipping/ice concentration. 
 
+    :param years: the range of years to animate for (from downloaded rasters, 2011-2020)
+    :param bounds: the bounds to crop rasters to
+    :param interval: The speed at which to animate the gif
+    :param shipping: boolean to include shipping information
+    :param ice: boolean to include ice information
+    :param save: boolean to save figure
+    :param filename: optional filename if save is True
+
+    :return anim: matplotlib animation function object
+    '''
+    #Get file extensions for both shipping and ice
     maritime = 'data/ais_traffic/arctic_maritime'
     ice_con = 'data/ice_concentration/ice_conc_nh_ease2-250_cdr-v3p1_'
     
+    #Retrieve Cartopy arcitc projection, bounds, and appropriate cbar scale for shipping data
     proj = ccrs.NorthPolarStereo(central_longitude=0, true_scale_latitude=71)
     extent = [bounds.left, bounds.right, bounds.bottom, bounds.top]
     norm = LogNorm(vmin=0.0001, vmax=2000)
 
+    #Generate figure and ax objects and set ax limits, give fig cartopy projection for spatial data
     fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': proj})
     ax.set_xlim(bounds.left, bounds.right)
     ax.set_ylim(bounds.bottom, bounds.top)
     
-    
+    #Manually add basemap features ocean, land, coastline, and borders
     ax.add_feature(cfeature.OCEAN.with_scale('50m'), facecolor="#717171", edgecolor='none', zorder=0)
     ax.add_feature(cfeature.LAND.with_scale('50m'), facecolor='#0e0e0e', edgecolor='none', zorder=1)
     ax.add_feature(cfeature.COASTLINE.with_scale('50m'), edgecolor='#333333', linewidth=0.5, zorder=2)
     ax.add_feature(cfeature.BORDERS.with_scale('50m'), edgecolor='#333333', linewidth=0.3, zorder=2)
 
-
-
+    #If ice is selected, add base to map
     if ice:
+        #Open shipping to get correct resolution
         with rio.open(f'{maritime}_{years[0]}.tif') as src:
             ship_res = src.res 
+        #Use xarray to open .nc ice file
         ds = xr.open_dataset(f'{ice_con}{years[0]}09.nc')
 
+        #Retrieve appropriate attribute (ice concentration), convert from km to m
         temp = ds['ice_conc'].isel(time=0)
         temp['xc'] = temp['xc'] * 1000
         temp['yc'] = temp['yc'] * 1000
+        #Set spatial dimensions
         temp = temp.rio.set_spatial_dims(x_dim='xc', y_dim='yc')
         temp.rio.write_crs('EPSG:6931', inplace=True)
 
+        #Project to arctic CRS
         temp_projected = temp.rio.reproject('EPSG:3995', resolution=ship_res)
         temp_clipped = temp_projected.rio.clip_box(minx=bounds.left,
                                                    miny=bounds.bottom,
                                                    maxx=bounds.right,
                                                    maxy=bounds.top)
+        #Use squeeze to remove unused attribute dimension, filter out 0 values as NA to avoid map clutter 
         img_arr_ice = temp_clipped.values.squeeze()
         img_arr_ice = np.where(img_arr_ice <= 0, np.nan, img_arr_ice)
-
+        #Add base image to gif
         im_ice = ax.imshow(img_arr_ice, cmap='Blues_r', extent=extent, norm=plt.Normalize(vmin=0,vmax=100), zorder=3)
-
+    
+    #Add shipping base image if present
     if shipping:
+        #Open shipping raster and add with correct normalizing for cbar
         with rio.open(f'{maritime}_{years[0]}.tif') as src:
             img_arr_mari = src.read(1)
 
         im_mari = ax.imshow(img_arr_mari, cmap='Reds_r', extent=extent, norm=norm, zorder=4)
 
+    #Draw figure and set axis for both shipping and ice, adjusting manually
     fig.canvas.draw()
     ax.set_position([0.02, 0.02, 0.84, 0.96])  # [left, bottom, width, height]
 
     #Shipping cbar
     cbar_ax = fig.add_axes([0.87, 0.08, 0.025, 0.40])
+    #Custom ticks for logarithmic scale
     cbar_ticks = [0.001, 0.05, 1, 2, 5, 10, 20, 100, 2000]
     cbar = plt.colorbar(im_mari, cax=cbar_ax, ticks=cbar_ticks, shrink=0.7)
     cbar.ax.set_yticklabels([str(i) for i in cbar_ticks])
@@ -131,13 +174,21 @@ def dynamic_plot(years, bounds, interval, shipping=True, ice=False, save=False, 
     ice_cbar = fig.colorbar(im_ice, cax=ice_cbar_ax)
     ice_cbar.set_label('Ice Concentration (%)', fontsize=12)
 
+    #Set blank placeholder title
     title = ax.set_title('', fontsize=14)
 
     
 
     def update(frame):
+        '''
+        Subfunction to update gif with new information
+
+        :param frame: new year for frame
+        '''
+        #create artists variable to hold title object
         artists = [title]
 
+        #Update ice raster with xarray, same process as before
         if ice:
             ds = xr.open_dataset(f'{ice_con}{frame}09.nc')
             temp = ds['ice_conc'].isel(time=0)
@@ -154,11 +205,12 @@ def dynamic_plot(years, bounds, interval, shipping=True, ice=False, save=False, 
             img_arr_ice = temp_clipped.values.squeeze()
             img_arr_ice = np.where(img_arr_ice <= 0, np.nan, img_arr_ice)
 
-
+            #Set new data
             im_ice.set_data(img_arr_ice)
 
             title.set_text(f'Arctic Maritime Traffic Density (AIS Data) - {frame}')
-                
+        
+        #Update shipping data
         if shipping:
             with rio.open(f'{maritime}_{frame}.tif') as src:
                 img_arr_mari = src.read(1)
@@ -166,40 +218,50 @@ def dynamic_plot(years, bounds, interval, shipping=True, ice=False, save=False, 
                 im_mari.set_data(img_arr_mari)
 
                 title.set_text(f'Arctic Maritime Traffic Density (AIS Data) - {frame}')
-        
+        #Return updated title, updated figure implicitly returned via matplotlib
         return artists
     
+    #Use update function to loop through years and update fig, save as anim object
     anim = FuncAnimation(fig, update, frames=years, interval=interval)
 
+    #Save if specified
     if save:
         anim.save(f'figures/{filename}', writer='pillow')
-
+    #Return function
     return anim
 
 
 def plot_lisa_clusters(clusters, title= 'LISA Clusters'):
+    '''
+    Given lisa classifications, plot data
+    '''
+    #Custom lisa colors and generated cmap
     lisa_colors = ['#eeeeee', '#d7191c', '#2c7bb6', '#abd9e9', '#fdae61']
     cmap = ListedColormap(lisa_colors)
 
+    #Retrieve appropriate bounds
     with rio.open('data/ais_traffic/arctic_maritime_2020.tif') as src:
         bounds = src.bounds
 
+    #Get cartopy basemap information
     proj = ccrs.NorthPolarStereo(central_longitude=0, true_scale_latitude=71)
     extent = [bounds.left, bounds.right, bounds.bottom, bounds.top]
-    norm = LogNorm(vmin=0.0001, vmax=2000)
 
+    #Generate fig and ax objects, set ax lims
     fig, ax = plt.subplots(figsize=(12, 12), subplot_kw={'projection': proj})
     ax.set_xlim(bounds.left, bounds.right)
     ax.set_ylim(bounds.bottom, bounds.top)
 
-
+    #Add basemap features
     ax.add_feature(cfeature.OCEAN.with_scale('50m'), facecolor="#717171", edgecolor='none', zorder=0)
     ax.add_feature(cfeature.LAND.with_scale('50m'), facecolor='#0e0e0e', edgecolor='none', zorder=1)
     ax.add_feature(cfeature.COASTLINE.with_scale('50m'), edgecolor='#333333', linewidth=0.5, zorder=2)
     ax.add_feature(cfeature.BORDERS.with_scale('50m'), edgecolor='#333333', linewidth=0.3, zorder=2)
 
+    #Add clusters with appropriate color
     im = ax.imshow(clusters, extent=extent, cmap=cmap, interpolation='nearest')
 
+    #Create custom legend
     from matplotlib.patches import Patch
     legend_elements = [
         Patch(facecolor='#eeeeee', label='Not Significant'),
@@ -210,5 +272,6 @@ def plot_lisa_clusters(clusters, title= 'LISA Clusters'):
     ]
     ax.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.3, 1))
     
+    #Title and display
     ax.set_title(title)
     plt.show()
--- processing.py
+++ processing.py
@@ -1,3 +1,8 @@
+#Helper module for data processing, including
+#1. API function for AIS data (ice concentration data downloaded over web)
+#2. Helper function to process ice raster
+#3. Function to manually calculate asympotic local/global spatial autocorrelation (avoids expensive permutation testing)
+#4. Function to classify pixels into lisa clusters
 import requests
 from PIL import Image
 import rasterio
@@ -11,50 +16,30 @@ import rioxarray
 from scipy.ndimage import convolve
 from scipy import stats
 
-def retrieve_ice_concentration(start_year, stop_year, type='tif', step=1, crs=3995):
-    
-
-    params = {
-        "dataset_id": "EO:ECMWF:DAT:SATELLITE_SEA_ICE_CONCENTRATION",
-        "variable": "all",
-        "sensor": "ssmis",
-        "region": [
-            "northern_hemisphere"
-        ],
-        "cdr_type": [
-            "cdr"
-        ],
-        "temporal_aggregation": "monthly",
-        "year": [
-            "2011",
-            "2012",
-            "2013",
-            "2014",
-            "2015",
-            "2016",
-            "2017",
-            "2018",
-            "2019",
-            "2020"
-        ],
-        "month": [
-            "09"
-        ],
-        "version": "3_1",
-        "itemsPerPage": 200,
-        "startIndex": 0
-        }
-    raise NotImplementedError
 
 def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, height=5201, crs=3995):
     '''
+    Function that retrieves AIS data for a given year range from API using bbox
 
     Example link:
     https://gmtds.maplarge.com/ogc/ais:density/wms?REQUEST=GetMap&LAYERS=ais:density&STYLES=&FORMAT=image/png&TRANSPARENT=true
     &SERVICE=WMS&VERSION=1.3.0&WIDTH=1600&HEIGHT=693&CRS=EPSG:3995
     &BBOX=-10115103.215710418,-4227137.379356397,9208953.375633113,3393787.4388547074&TIME=2023-04-01T00:00:00Z
     &CQL_FILTER="category_column='All' AND category='All'"
+
+    :param bbox: bounding box for query
+    :param start_year: year to begin querying by
+    :param stop_year: year to query to
+    :param type: file type desired, png or tif
+    :param step: step for querying (optional)
+    :param width: width of image to be returned, default matches resolution
+    :param height: height of image to return, default matches resolution
+    :param crs: crs to projet into, default arctic
+
+    Saves AIS directly to appropriate data folder. 
     '''
+    #Establish global calibration colors known from online legend.
+    #Data returns in PNG format, need to convert from RGB to actual AIS data. These colors are associated with subsequent AIS density (ship hours per km^2)
     CALIBRATION = [
         ('#cadeb9', 0.001),
         ('#c4d3a2', 0.05),
@@ -68,17 +53,27 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
         ('#61001f', 100),
         ('#3d0215', 2000)
     ]
+
     def hex_to_rgb(hex):
         '''
+        Helper function that converts hex code to RGB
+
+        :param hex: hex code
         '''
+        #Strip #, use formula to return 3 value point for R, G, B
         h = hex.strip('#')
         return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
+    #Get calibration colors and values from dict.
     cal_colors = np.array([hex_to_rgb(h) for h, _ in CALIBRATION], dtype=np.float32)
     cal_values = np.array([v for _, v in CALIBRATION], dtype=np.float32)
 
     def decode(png_array, threshold=25.):
         '''
+        Helper function to translate APi returned PNG to actual AIS data geotiff
+
+        :param png_array: 3D array with RGB information
+        :param threshold: threshold to clean up distorted pixels that are far from known color ramp
         '''
         #Get height and width of png, pixel values, and transparent pixels (RGBA format)
         h, w, _ = png_array.shape
@@ -91,9 +86,11 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
 
         # Vectorized segment-by-segment calculation with np
         for i in range(len(cal_colors) - 1):
+            #Get calibration values and colors
             A, B = cal_colors[i], cal_colors[i+1]
             v1, v2 = cal_values[i], cal_values[i+1]
             
+            #Calculate vector from difference and dot product, skip if difference is 0 (avoids errors)
             vec = B - A
             mag_sq = np.dot(vec, vec)
             if mag_sq == 0: continue
@@ -107,7 +104,7 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
             closest_pts = A + t[:, np.newaxis] * vec
             dist = np.linalg.norm(pixels - closest_pts, axis=1)
             
-            #Update pixels where this segment is the closest found so far
+            #Update pixels where this segment is the closest found so far, otherwise keep same
             mask = dist < min_distances
             min_distances[mask] = dist[mask]
             
@@ -118,18 +115,23 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
             interp_vals = np.exp(log_v1 + t[mask] * (log_v2 - log_v1))
             final_values[mask] = interp_vals
 
-        # Clean up, mask pixels that are too far from the ramp or transparent
+        # Clean up, mask pixels that are too far from the ramp or transparent (will be recalculated on next iteration)
         invalid_mask = (min_distances > threshold) | (alpha == 0)
         final_values[invalid_mask] = np.nan
         
+        #Return reshaped final values for each pixel
         return final_values.reshape(h, w)
     
+    #Base for requests
     base = 'https://gmtds.maplarge.com/ogc/ais:density/wms'
 
+    #loop through specified year
     for year in range(start_year, stop_year+1, step):
         time.sleep(1)
+        #Generate correct timestamp for September
         timestamp = f'{year}-09-01T00:00:00Z'
         
+        #Setup param dictionary for API for appropriate get request
         params = {
                 'REQUEST': 'GetMap',
                 'LAYERS': 'ais:density',
@@ -146,19 +148,20 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
                 'CQL_FILTER': "category_column='All' AND category='All'"
             }
 
+        #Set correct file format for saving
         if type == 'tif':
             filename = f'arctic_maritime_{year}.tif'
         else:   
             filename = f'arctic_maritime_{year}.png'
 
-
+        #Send get request
         print(f'Requesting data for {year}:')
         try:
             response = requests.get(base, params=params, timeout=40)
             if response.status_code==200:
                 
                 if type == 'tif':
-
+                    #Open png with PIL, use decode function.
                     png = Image.open(io.BytesIO(response.content)).convert('RGBA')
                     print('Decoding RGB values to AIS Traffic')
                     data = decode(np.array(png))
@@ -166,9 +169,10 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
                     print("Max:", np.nanmax(data))
                     print("Mean:", np.nanmean(data))
                     print("NaN %:", np.isnan(data).mean() * 100)
-
+                    #Transform array to correct bounds 
                     transform = from_bounds(bbox[0], bbox[1], bbox[2], bbox[3], width, height)
 
+                    #Open with rasterio, use transformation and appropriate crs to reproject decoded data into appropriate tif format
                     #with rasterio.Env(GDAL_PAM_ENABLED="NO"):
                     with rasterio.open(
                         f'data/ais_traffic/{filename}', 
@@ -182,49 +186,71 @@ def retrieve_ais(bbox, start_year, stop_year, type='tif', step=1, width=5952, he
                         transform=transform
                     ) as file:
                         file.write(data, 1)
+                #If png, just write what is retrieved
                 else:
                     with open(f'data/ais_traffic/{filename}', 'wb') as file:
                         file.write(response.content)
                 print(f'{year} successfully downloaded.')
+            #{Print response code}
             else:
                 print(f'Error: {response.status_code}')
+        #Print exception
         except Exception as e:
             print(f'Failed: {e}')
 
 
 def process_ice_raster(path):
+    '''
+    Helper function to simplify ice raster processing
+
+    :param path: path to ice concentration raster
+    '''
+    #Get resolution and bounds from shipping image
     maritime = 'data/ais_traffic/arctic_maritime'
     with rasterio.open(f'{maritime}_{2012}.tif') as src:
                 ship_res = src.res 
                 bounds = src.bounds
+    #Open path with xarray package to read .nc file format 
     ds = xr.open_dataset(path)
 
+    #Retrieve appropriate attribute, convert to km to m, set spatial dimensions 
     temp = ds['ice_conc'].isel(time=0)
     temp['xc'] = temp['xc'] * 1000
     temp['yc'] = temp['yc'] * 1000
     temp = temp.rio.set_spatial_dims(x_dim='xc', y_dim='yc')
     temp.rio.write_crs('EPSG:6931', inplace=True)
 
+    #Reproject to arctic crs
     temp_projected = temp.rio.reproject('EPSG:3995', resolution=ship_res)
     temp_clipped = temp_projected.rio.clip_box(minx=bounds.left,
                                                 miny=bounds.bottom,
                                                 maxx=bounds.right,
                                                 maxy=bounds.top)
+    
+    #Remove unecessary attribute dimension, mask 0 values as NA to avoid clutter, return
     img_arr = temp_clipped.values.squeeze()
     img_arr = np.where(img_arr <= 0, np.nan, img_arr)
     return img_arr
 
 def spatial_autocorrelation(raster):
+    '''
+    Function to determine global and local spatial autocorrelation for a given raster. 
+
+    :param raster: input raster. 
+    '''
+    #Mask out NA values for calculation
     mask = ~np.isnan(raster)
     n = np.sum(mask)
     data = np.nan_to_num(raster, nan=0.0)
 
+    #Extract means and standard devation to get z score for data
     means = data[mask].mean()
     std = data[mask].std()
     z = (data - means)/std
 
     z[~mask]=0
 
+    #Establish kernel for local spatial autocorrelation, 'queens distance' 8 surrounding cells
     kernel = np.array([[1, 1, 1],
                        [1, 0, 1],
                        [1, 1, 1]])
@@ -251,21 +277,32 @@ def spatial_autocorrelation(raster):
     local_variance = (w_i2 * (n - b2)) / (n - 1)
     e_i = (w_i2 * (n-b2))/(n-1)
 
+    #Calculate local z and p values based on above variance and epxectation calculation (for each pixel), using NP
     local_z = (local_morans - e_i) / np.sqrt(local_variance)
     local_p = 2 * (1 - stats.norm.cdf(np.abs(local_z)))
 
+    #Mask NA values
     local_p = np.where(mask, local_p, np.nan)
     
+    #Determine lisa clusters and return values
     clusters = lisa_clusters(z, spatial_lag)
 
     return global_morans, local_morans, local_p, clusters
 
+
 def lisa_clusters(z, spatial_lag, p_values=None, alpha=0.01):
+    '''
+    Function to classify pixels into local indication of spatial autocorrelation (LISA) clusters
+
+    :param z: local z score (Moran's I statistic)
+    :param spatial_lag: spatial lag for each pixel
+    '''
+
     #Define empty array for hotspot, coldspot categories
     # 0 (not significant, optional), 1: HH, 2: LL, 3: LH, 4: HL
     lisa_clusters = np.zeros(z.shape, dtype=int)
 
-    #Define quadrants
+    #Define quadrants based on z and spatial lag
     hh = (z>0) & (spatial_lag > 0)
     ll = (z < 0) & (spatial_lag < 0)
     lh = (z < 0) & (spatial_lag > 0)
@@ -279,4 +316,5 @@ def lisa_clusters(z, spatial_lag, p_values=None, alpha=0.01):
     if p_values is not None:
         lisa_clusters[p_values > alpha] = 0
     
+    #Return classified clusters
     return lisa_clusters
